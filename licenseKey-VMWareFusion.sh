2d11a302565749a2d3ef94a21b3b2718142fe32a
Christopher Scott <chrisjscott@users.noreply.github.com>
2018-02-09 10:39:50 -0500

    Update licenseKey-VMWareFusion.sh
    added '-tr' flag to ls command to ensure that most recent license file is used

--- licenseKey-VMWareFusion.sh
+++ licenseKey-VMWareFusion.sh
@@ -3,11 +3,13 @@
 #
 # This script checks for the existence of a VMware Fusion directory - if one exists then
 # it selects the latest version's license file and outputs the serial number and version.
+#
+# 18.02.09: added '-tr' flag to ls command to ensure that most recent license file is used
 
 licenseFilePath="/Library/Preferences/VMware Fusion"
 
 if [ -d "$licenseFilePath" ]; then
-   latestLicenseFile=$(ls "$licenseFilePath"/license-fusion-* | tail -n 1)
+   latestLicenseFile=$(ls -tr "$licenseFilePath"/license-fusion-* | tail -n 1)
 
    sn=`cat "$latestLicenseFile" | grep "Serial =" | awk '{print $3}' | sed 's/"//g'`
    version=`cat "$latestLicenseFile" | grep "LicenseVersion =" | awk '{print $3}' | sed 's/"//g'`
